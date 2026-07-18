8abba4f79e3f7e52d563e13614b0259b4ab0f64b
Santolass06 <andremsantos06@duck.com>
2026-07-18 22:35:15 +0100

    fix: self-host fontes (Hanken Grotesk, Newsreader, IBM Plex Mono, Material Symbols)
    Eram carregadas via Google Fonts CDN. Numa máquina sem internet no
    arranque (testado numa build limpa via .deb), as fontes falhavam a
    carregar e os ícones apareciam como nome literal do glifo em vez do
    símbolo (ex.: "euro", "warning", "check_circle").

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { I18nProvider } from "./i18n";
 import { applyTheme } from "./theme";
+import "@fontsource/hanken-grotesk/400.css";
+import "@fontsource/hanken-grotesk/500.css";
+import "@fontsource/hanken-grotesk/600.css";
+import "@fontsource/hanken-grotesk/700.css";
+import "@fontsource/newsreader/400.css";
+import "@fontsource/newsreader/400-italic.css";
+import "@fontsource/newsreader/500.css";
+import "@fontsource/newsreader/600.css";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/ibm-plex-mono/600.css";
+import "material-symbols/outlined.css";
 import "./styles/theme.css";
 
 const initTheme = async () => {
